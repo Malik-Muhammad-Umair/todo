bcfbf85d92b6fc2fc38b3da6eccc45202b3de403
Malik-Muhammad-Umair <malikmumair13@gmail.com>
2025-07-19 12:35:42 -0800

    added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,5 +7,6 @@ import { ContactComponent } from './myComponent/contact/contact.component';
 export const routes: Routes = [
   { path: '', component: TodosComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'contact', component: ContactComponent }
+  { path: 'contact', component: ContactComponent },
+  // { path: '**',redirectTo:'', component: TodosComponent }
 ];
